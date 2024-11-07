4c773a9812731592a20de691fc65b5e6bae4ca2f
odinokun <odinokun@gmail.com>
2024-11-06 20:46:47 -0500

    Add Cars component to display a list of top cars

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import { Cars } from './Cars';
+
+const topCars = [
+  { manufacturer: 'BMW', model: 'm5cs' },
+  { manufacturer: 'Mercedes', model: 'e63s' },
+  { manufacturer: 'Audi', model: 'rs6' },
+];
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Cars cars={topCars} />
     </>
   );
 }
