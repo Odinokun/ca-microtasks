4bb6b3e2ed2703fd60e2fb711fe4a2a0a5414645
odinokun <odinokun@gmail.com>
2024-11-06 19:56:16 -0500

    Remove nested components: Header, Body, and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import { Header } from './01-nested-components/Header';
-import { Body } from './01-nested-components/Body';
-import { Footer } from './01-nested-components/Footer';
 
 function App() {
   return (
     <>
-      <Header title='New header' />
-      <Body title='New body' />
-      <Footer title='New footer' />
+      <h1>App</h1>
     </>
   );
 }
