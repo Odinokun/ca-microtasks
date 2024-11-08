d836cb9092332282089afa331e0a304fd6eed769
odinokun <odinokun@gmail.com>
2024-11-08 17:00:34 -0500

    Remove onClickHandler and simplify button components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
 
 function App() {
-  const onClickHandler = (name: string) => {
-    console.log('My name is: ', name);
-  };
-
   return (
     <>
-      <button onClick={() => onClickHandler('Bob')}>MyYouTubeChannel-1</button>
-
-      <button onClick={() => onClickHandler('Dima')}>MyYouTubeChannel-2</button>
+      <button>MyYouTubeChannel-1</button>
+      <button>MyYouTubeChannel-2</button>
     </>
   );
 }
