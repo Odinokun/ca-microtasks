6508519bcb167cd37db8636961eccdae14532777
odinokun <odinokun@gmail.com>
2024-11-09 11:05:32 -0500

    Remove counter functionality from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [a, setA] = useState(1);
-
-  const onClickHandler = () => {
-    setA(++a);
-  };
-
-  const resetCounter = () => {
-    setA(0);
-  };
-
   return (
     <div className='App'>
-      <h1>{a}</h1>
-      <button onClick={onClickHandler}>count ++</button>
-      <button onClick={resetCounter}>reset</button>
+      <h1>App</h1>
     </div>
   );
 }
