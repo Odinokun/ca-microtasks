ecb70bec7184792bf102c80ba5167c3467b8bfc2
odinokun <odinokun@gmail.com>
2024-11-15 16:54:43 -0500

    Add message state and input functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
+type MessageType = {
+  id: number;
+  message: string;
+};
+
 function App() {
+  const [message, setMessage] = useState<MessageType[]>([
+    { id: 1, message: 'My message 1' },
+    { id: 2, message: 'My message 2' },
+    { id: 3, message: 'My message 3' },
+  ]);
+
   return (
     <div className='App'>
-      <h1>app</h1>
+      <div>
+        <div>
+          <input type='text' />
+          <button>Click me</button>
+        </div>
+
+        {message.map(m => (
+          <div key={m.id}>{m.message}</div>
+        ))}
+      </div>
     </div>
   );
 }
